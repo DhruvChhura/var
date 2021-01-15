774d678909c95365f6fe95500dfce97b51a3fab8
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-01-15 21:30:30 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,16 +1,16 @@
 # Repo URLs
-rom_manifest_url="https://github.com/Corvus-ROM/android_manifest.git"
-rom_manifest_branch="11"
+rom_manifest_url="https://github.com/PixelExperience/manifest"
+rom_manifest_branch="eleven"
 
 # Local manifest (dt,vt,kt,dependencies will sync from here)
 local_manifest_url="https://github.com/DhruvChhura/android_.repo_local_manifests"
 local_manifest_branch="DhruvChhura-patch-1"
 
 # Lunch device
-lunch="lunch corvus_ysl-userdebug"
+lunch="lunch aosp_ysl-userdebug"
 
 # build command
-build_command="make corvus"
+build_command="mka bacon"
 
 # Telegram Bot
 your_telegram_id="1367251045"
