beceae4f43af0adbfe064465df7d9fc88ed6977f
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-01-15 14:24:05 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,16 +1,16 @@
 # Repo URLs
-rom_manifest_url="https://github.com/PixelExperience/manifest"
-rom_manifest_branch="eleven"
+rom_manifest_url="git://github.com/ProjectSakura/android.git"
+rom_manifest_branch="11"
 
 # Local manifest (dt,vt,kt,dependencies will sync from here)
 local_manifest_url="https://github.com/DhruvChhura/android_.repo_local_manifests"
 local_manifest_branch="DhruvChhura-patch-1"
 
 # Lunch device
-lunch="lunch aosp_ysl-userdebug"
+lunch="lunch lineage_ysl-userdebug"
 
 # Build command
-build_command="make bacon"
+build_command="mka bacon -jx"
 
 # Telegram Bot
 your_telegram_id="1367251045"
