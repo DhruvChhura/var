dd0a17779a52b6c492f8c6af617db0140a3efaf0
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-01-16 16:09:04 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,6 +1,6 @@
 # Repo URLs
-rom_manifest_url="https://github.com/PixelExperience/manifest"
-rom_manifest_branch="eleven"
+rom_manifest_url="git://github.com/AospExtended/manifest.git"
+rom_manifest_branch="11.x"
 
 # Local manifest (dt,vt,kt,dependencies will sync from here)
 local_manifest_url="https://github.com/DhruvChhura/android_.repo_local_manifests"
@@ -10,7 +10,7 @@ local_manifest_branch="DhruvChhura-patch-1"
 lunch="lunch aosp_ysl-userdebug"
 
 # Build command
-build_command="mka bacon"
+build_command="make aex"
 
 # Telegram Bot
 your_telegram_id="1367251045"
