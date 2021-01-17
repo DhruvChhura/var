008f16fd2060dd04eb749efeefd6fc12c8f65261
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-01-17 13:58:35 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,19 +1,16 @@
 # Repo URLs
-rom_manifest_url="git://github.com/AospExtended/manifest.git"
-rom_manifest_branch="11.x"
+rom_manifest_url="https://github.com/PotatoProject/manifest"
+rom_manifest_branch="dumaloo-release"
 
 # Local manifest (dt,vt,kt,dependencies will sync from here)
 local_manifest_url="https://github.com/DhruvChhura/android_.repo_local_manifests"
 local_manifest_branch="DhruvChhura-patch-1"
 
-# Env setup
-Env_setup=". build/envsetup.sh"
-
 # Lunch device
-lunch="lunch aosp_ysl-userdebug"
+lunch="lunch potato_ysl-userdebug"
 
 # build command
-build_command="make aex"
+build_command="brunch ysl"
 
 # Telegram Bot
 your_telegram_id="1367251045"
