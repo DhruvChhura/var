0a615220325b43b664a17095bc033c10021d7ce2
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-01-27 09:40:29 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,16 +1,16 @@
 # Repo URLs
-rom_manifest_url="https://github.com/PotatoProject/manifest"
-rom_manifest_branch="dumaloo-release"
+rom_manifest_url="https://github.com/ArrowOS/android_manifest.git"
+rom_manifest_branch="arrow-11.0"
 
 # Local manifest (dt,vt,kt,dependencies will sync from here)
 local_manifest_url="https://github.com/DhruvChhura/android_.repo_local_manifests"
 local_manifest_branch="DhruvChhura-patch-1"
 
 # Lunch device
-lunch="lunch potato_ysl-userdebug"
+lunch="lunch arrow_ysl-userdebug"
 
 # Build command
-build_command="brunch potato_ysl-userdebug"
+build_command="m bacon"
 
 # Telegram Bot
 your_telegram_id="1367251045"
